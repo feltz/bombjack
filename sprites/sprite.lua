4c32725fe477c0455925f0031f91067a6461082f
fongor74 <feltz@chez.com>
2021-05-07 23:26:52 +0200

    Fixes mummy collision, life lost view and Esc to stop game

--- sprites/sprite.lua
+++ sprites/sprite.lua
@@ -4,6 +4,10 @@ function Sprite:new (x, y, w, h)
   self.x, self.y, self.w, self.h = x, y, w, h
 end
 
+function Sprite:ignoreCollision()
+  return false
+end
+
 function Sprite:__tostring()
   return "<Sprite: x="..tostring (round(self.x))..', y='..tostring (round(self.y))..', w='..tostring (self.w)..', h='..tostring (self.h)..">"
 end
